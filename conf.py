fcd7ac546e11f2d3e1275ac1947f8c69f0118168
Thomas Tu <ttu@lbl.gov>
2016-02-10 18:21:10 -0800

    add default source extractor config

--- conf.py
+++ conf.py
@@ -2,3 +2,5 @@ import os
 
 PROJECT_DIR = os.path.abspath(os.path.dirname(__file__))
 FIXTURE_DIR = os.path.join(PROJECT_DIR, "flipp", "fixtures")
+
+SEXCONFPATH = os.path.join(PROJECT_DIR, "flipp", "libs", "sextractor")
