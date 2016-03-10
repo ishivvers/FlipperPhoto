fb872033e10d57e7cffe69d13b33e4d4727f9b67
Thomas Tu <ttu@lbl.gov>
2016-03-09 18:53:59 -0800

    rudimetary working wrapper

--- flipp/libs/sextractor/wrapper.py
+++ flipp/libs/sextractor/wrapper.py
@@ -2,13 +2,28 @@
 """Python wrapper for Sextractor, with python-friendly config."""
 import os
 import re
+
 from tempfile import mktemp
+
+from astropy.io.fits import hdu
+from astropy.table import Table
+
 
 from flipp.libs.utils import ShellCmd
 from conf import SEXCONFPATH
 
+# ================
+# DEVELOPMENT NOTE
+# ================
+# WE SHOULD CREATE A /TMP/ FOR EACH OF THESE AS WELL WITH USER-SPECIFIED 
+# CONFIGURATION.  THERE OUGHT TO BE A CONFIG METHOD FOR EACH OF THESE
+# ALTERNATIVELY, YOU COULD STAY AGNOSTIC AND ASK FOR USER-SPECIFIED FILE
+# IMPLEMENTATION WISE, THIS WOULD REQUIRE HAVING THE ABILITY TO DO THE OPPOSITE
+# OF ``sextractorConfig._sexconf2dict``.  THIS IS TEDIOUS SO DO IT LATER.
+
 default_sex = os.path.join(SEXCONFPATH, "default.sex")
 default_param = os.path.join(SEXCONFPATH, "default.param")
+default_filter = os.path.join(SEXCONFPATH, "gauss_3.0_5x5.conv")
 
 class sextractorConfig(object):
 
@@ -16,7 +31,7 @@ class sextractorConfig(object):
     value_re = re.compile("\S*(?=\s)")
     section_re = re.compile("\#(\-+)(\w|\s)+")
 
-    def _conf2dict(self, path=default_sex):
+    def _sexconf2dict(self, path=default_sex):
         with open(path) as f:
             d = dict(filter(None, map(self._parse_sextractor_option, f.readlines())))
         return d
@@ -26,34 +41,69 @@ class sextractorConfig(object):
         match = self.option_re.match(s)
         if match:
             parts = re.split('\s+', s, 1)
-            return match.group(), self.value_re.match(parts[1]).group()
+            return match.group(), \
+                '"%s"' %(self.value_re.match(parts[1]).group())
         return tuple()
 
-class sextractor(ShellCmd):
+class Sextractor(sextractorConfig, ShellCmd):
     """Sextractor wrapper with config built in."""
 
     cmd = "sextractor"
 
-    @property
-    def tmpfp(self):
-        return 
-
     def set_defaults(self):
         """Sets default settings for running sextractor."""
-        D = {"CATALOG_NAME" : mktemp(".txt"), # Catalog
+
+        D = {"CATALOG_NAME" : mktemp(suffix=".txt", prefix="CATALOG_"), # Catalog
             "CHECKIMAGE_TYPE" : "OBJECTS,BACKGROUND", # Objects
-            "CHECKIMAGE_NAME" : "%s,%s" %(mktemp(".fits"), mktemp(".fits")),
-            "c" : "default.sex",
-            "PARAMETERS_NAME" : "default.param",
-            "FILTER_NAME" : "gauss_3.0_5x5.conv",
+            "CHECKIMAGE_NAME" : "%s,%s" %(mktemp(suffix=".fits", prefix="CHECK-OBJECTS_"), mktemp(suffix=".fits", prefix="CHECK-BKGRND_")),
+            "FILTER_NAME" : default_filter,
+            "PARAMETERS_NAME" : default_param,
+            "c" : default_sex,
             }
         return D
 
-    def fits2disk(self, buffer, fp):
-        raise NotImplementedError
+    def getimgfile(self, filepath_or_buffer):
+        fp = filepath_or_buffer
+        if isinstance(fp, str): return fp
+        if isinstance(fp, file): return fp.name
+        if isinstance(fp, hdu.hdulist.HDUList) or \
+            isinstance(fp, hdu.image.PrimaryHDU):
+            z = mktemp(suffix=".fits")
+            fp.writeto(z)
+            return z
 
-    def extract(self, pathname_or_fits, **kwargs):
-        """Write to filepath or buffer."""
+    def get_output_filepaths(self, dict_config):
+        return {"catalog" : dict_config.get('CATALOG_NAME', None),
+            "check_imgs" : dict_config.get('CHECKIMAGE_NAME').split(',')}
+
+    def extract(self, filepath_or_buffer,**kwargs):
+        """Run source-extractor (sextractor) on the given image.
+
+        Note
+        ----
+        Currently, CHECK_IMGS are deleted.  This is fairly easy to change
+        if we decide to actually use them for something.
+
+        Return
+        ------
+
+        """
         options = self.set_defaults()
         options.update(kwargs)
-        self.run()
+        img_file = self.getimgfile(filepath_or_buffer)
+        stdout = self.run(img_file, **options)
+        
+        # ===========================================================
+        # Delete all CHECK Images for now, it doesn't seem like we're
+        # using them.  If we want to use them, then modify this block
+        for c in options.get("CHECKIMAGE_NAME").split(","):
+            os.remove(c)
+        # ===========================================================
+        catalog = Table.read(options.get("CATALOG_NAME"), format="ascii.sextractor")
+        # Cleanup catalog file
+        os.remove(options.get("CATALOG_NAME"))
+        return catalog
+
+class Astrometry(ShellCmd):
+
+    pass
