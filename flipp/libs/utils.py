8e3bb3c48d97d2b83fbf430a3cee322d05152da2
Thomas Tu <ttu@lbl.gov>
2016-03-23 12:36:22 -0700

    light refactoring of basic config options for bash calls, consider luigi?

--- flipp/libs/utils.py
+++ flipp/libs/utils.py
@@ -1,4 +1,9 @@
+# -*- coding : utf-8 -*-
+import os
+
+from astropy.io.fits import hdu
 from subprocess import Popen, PIPE
+from tempfile import mktemp
 
 class ShellCmd(object):
     """Generic bash command wrapper with some option/argument parsing and giving
@@ -6,7 +11,7 @@ class ShellCmd(object):
 
     Note
     ----
-    This is meant to be subclassed with a :attr:`cmd` value that 
+    This is meant to be subclassed with a :attr:`cmd` value that
     corresponds to a valid bash command.  Commands run remotely may
     contain global side effects such as file creation.  The subclass
     should handle any expected side effects, retriving remote files
@@ -25,10 +30,27 @@ class ShellCmd(object):
         if kwargs:
             pieces = [' '.join([k, '"%s"' %(v)]) for k, v in kwargs.iteritems()]
             cmdkwargs = " ".join(["-%s" %(s) for s in pieces])
-            
+
         base = " ".join((base, cmdargs, cmdkwargs))
         return base
 
     def run(self, *args, **kwargs):
         cmd = self.configure(*args, **kwargs)
         return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
+
+class IMI(object):
+    """In-Memory-Image mixin"""
+
+    def getimgdir(self, filepath_or_buffer):
+        f = self.getimgfile(filepath_or_buffer)
+        return os.path.dirname(f)
+
+    def getimgfile(self, filepath_or_buffer):
+        fp = filepath_or_buffer
+        if isinstance(fp, str): return fp
+        if isinstance(fp, file): return fp.name
+        if isinstance(fp, hdu.hdulist.HDUList) or \
+            isinstance(fp, hdu.image.PrimaryHDU):
+            z = mktemp(suffix=".fits")
+            fp.writeto(z)
+            return z
