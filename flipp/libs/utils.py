0e3793dd96d65ea3d1877477cbf65e936e140fa5
Thomas Tu <ttu@lbl.gov>
2016-03-30 14:13:55 -0700

    some changes to how args/kwargs are read in shell wrapper

--- flipp/libs/utils.py
+++ flipp/libs/utils.py
@@ -2,6 +2,7 @@
 import os
 
 from astropy.io.fits import hdu
+from astropy.io import fits
 from subprocess import Popen, PIPE
 from tempfile import mktemp
 
@@ -20,9 +21,24 @@ class ShellCmd(object):
 
     cmd = ""
 
-    def configure(self, *args, **kwargs):
+    @classmethod
+    def update_args(self, defaults, update_args):
+        d = list(defaults)
+        for u in update_args:
+            if u not in defaults:
+                d.append(u)
+        return d
+
+    @classmethod
+    def update_kwargs(self, defaults, update_kwargs):
+        d = dict(defaults)
+        d.update(update_kwargs)
+        return d
+
+    @classmethod
+    def configure(cls, *args, **kwargs):
         """."""
-        base = str(self.cmd)
+        base = str(cls.cmd)
         cmdargs, cmdkwargs = "", ""
 
         if args:
@@ -34,17 +50,20 @@ class ShellCmd(object):
         base = " ".join((base, cmdargs, cmdkwargs))
         return base
 
-    def run(self, *args, **kwargs):
-        cmd = self.configure(*args, **kwargs)
+    @classmethod
+    def run(cls, *args, **kwargs):
+        cmd = cls.configure(*args, **kwargs)
         return Popen(cmd, shell=True, stdout=PIPE, stderr=PIPE).communicate()
 
 class IMI(object):
     """In-Memory-Image mixin"""
 
+    @classmethod
     def getimgdir(self, filepath_or_buffer):
         f = self.getimgfile(filepath_or_buffer)
         return os.path.dirname(f)
 
+    @classmethod
     def getimgfile(self, filepath_or_buffer):
         fp = filepath_or_buffer
         if isinstance(fp, str): return fp
@@ -54,3 +73,23 @@ class IMI(object):
             z = mktemp(suffix=".fits")
             fp.writeto(z)
             return z
+
+class ZCat(ShellCmd):
+
+    cmd = "zcat"
+
+    @classmethod
+    def open(cls, filepath):
+        raw = cls.run(filepath)[0]
+        hdu = fits.open(StringIO(raw))
+        hdu.verify("fix")
+        return hdu
+
+def open_fits(fp):
+
+    try:
+        hdu = fits.open(fp)
+    except IOError:
+        hdu = ZCat.open(fp)
+
+    return hdu
