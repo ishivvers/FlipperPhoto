59d4d4654b559f61ccad4d74067e7b7a2bd13fe1
Thomas Tu <ttu@lbl.gov>
2016-04-06 17:02:30 -0700

    fix typos

--- flipp/libs/astrometry/wrapper.py
+++ flipp/libs/astrometry/wrapper.py
@@ -6,6 +6,17 @@ coordinate information based on input telescope characteristics.
 
 This module provides a python interface for the astrometry.net software.
 Host machine must have a astrometry.net installation for this to work.
+
+Note
+----
+
+Depending on the longevity of this piece of software, it is worth
+noting that further development should be done in such a way that
+photutils can be easily integrated as a drop-in replacement when
+the photutils project is mature enough to replace astrometry.
+
+It should be mentioned that a lot of the code design was derived
+from studying the structure of the LEMON project.
 """
 
 import os
@@ -15,7 +26,7 @@ from tempfile import mktemp
 
 from astropy.io import fits
 
-from flipp.libs.utils import ShellCmd
+from flipp.libs.utils.shell import shMixin, FitsIOMixin
 from conf import SEXCONFPATH
 
 DEFAULT_CONF = "/usr/local/astrometry/etc/astrometry.cfg"
@@ -23,16 +34,12 @@ DEFAULT_CONF = "/usr/local/astrometry/etc/astrometry.cfg"
 put under version control in this repository.  Let this be
 temporary for now."""
 
-class ConfigurationError(Exception):
 
-    pass
+class Astrometry(shMixin, FitsIOMixin):
 
-class Astrometry(ShellCmd, object):
-
-    cmd = "solve-field" # cmd attribute for ShellCmd mixin
-
+    cmd = "solve-field" # For shMixin
     required_config_keys = ("pixscaleL",
-        "pixscaleH",)
+        "pixscaleH",) # For FitsIOMixin
 
     def __init__(self, fp_or_buffer, telescope_config):
         """
@@ -41,85 +48,12 @@ class Astrometry(ShellCmd, object):
         the actual image on disk is, and to never actually write on top
         or over that image.
         """
-        name, path, image = self.__parse_input(fp_or_buffer)
-        telescope_config = self.__parse_telescope_config(telescope_config)
+        name, path, image = self._parse_input(fp_or_buffer)
+        telescope_config = self._parse_telescope_config(telescope_config)
         self.name = name
         self.path = path
         self.image = image
-        self.telescope = telescope_dict
-
-    def __parse_input(self, obj, inplace=False):
-        """Sets instance attributes based on input type.
-
-        If obj is string-like, assume it is a filepath, and open it as an
-        astropy HDUList.
-
-        If obj is already an astropy HDUList, stay agnostic about origin of
-        file on disk, and create a temp file for it. This is potentially
-        very slow, but ensures that original images are never written over.
-        """
-        if isinstance(obj, basestring):
-            # Handle filepaths
-            path = obj
-            try:
-                image = fits.open(obj)
-            except IOError:
-                self.image = ZCat.open(obj)
-            name = os.path.split(path)[1]
-
-        elif isinstance(obj, fits.hdu.hdulist.HDUList):
-            # or isinstance(obj, fits.hdu.image.PrimaryHDU):
-            # Add handling for this if we want to pass in a non-HDUList object
-            image = obj
-            fp = obj.filename()
-            if inplace and fp:
-                path = fp
-            else:
-                z = mktemp(suffix=".fits")
-                obj.writeto(z)
-                path = z
-            if fp:
-                name = os.path.split(fp)[1]
-            else:
-                name = os.path.split(path)[1]
-
-        else:
-            # Aggressive type-checking for unhandled inputs
-            excp = ("Unparseable object type %s.  Obj must be a filepath or"
-                "astropy fits.open object.") %(type(obj))
-            raise TypeError(excp)
-
-        return name, path, image
-
-    def __parse_telescope_config(self, obj):
-        if isinstance(obj, basestring):
-            # Assume someone has set up a dictionary in conf.py
-            try:
-                config = dict(conf['TELESCOPES'][obj])
-            except AttributeError as e:
-                excp = "'%s' is not configured in TELESCOPES configuration."
-                raise ConfigurationError(excp %(obj))
-        elif isinstance(obj, dict):
-            config = dict(obj)
-        else:
-            excp = "Telescope Configuration must be one of %s or dictionary."
-            preset = list(conf['TELESCOPES'].keys())
-            raise TypeError(excp %(', '.join(['"%s"' %(x) for x in preset])))
-
-        self.validate_config(config)
-        return config
-
-    def validate_telescope_config(self, d):
-        """Checks for required keys.
-
-        Future iterations should also do some type-checking and
-        check that values are reasonable.
-        """
-        for k in self.required_config_keys:
-            if k not in d:
-                excp = "Improperly configured telescope.  Missing %s."
-                raise ConfigurationError(excp %(k))
-            else: pass
+        self.telescope = telescope_config
 
     @property
     def defaults(self):
@@ -128,8 +62,8 @@ class Astrometry(ShellCmd, object):
             '-tweak-order' : 2,
             '-overwrite' : '',
             '-no-plots' : '',
-            '-use-sextractor' : '',
-            '-sextractor-path' : '"%s%"' %(SEXPATH),
+            #'-use-sextractor' : '',
+            #'-sextractor-config' : '"%s"' %(SEXCONFPATH),
             "-ra" : self.image[0].header["RA"],
             "-dec" : self.image[0].header["DEC"],
             "-radius" : 0.3,
@@ -138,19 +72,31 @@ class Astrometry(ShellCmd, object):
             "-dir" : os.path.dirname(os.path.abspath(self.path)),
             "-new-fits" : mktemp(suffix=".fits",
                 prefix = "SOLVED-%s" %(self.name)),
+            "-cpulimit" :  10,
             }
         return default_values
 
-    def get_output(self, config_dict):
+    def get_output_path(self, config_dict):
         filedir = config_dict['-dir']
         filename = config_dict['-new-fits']
         return os.path.join(filedir, filename)
 
+    def get_solved_img(self, config_dict):
+        filedir = config_dict['-dir']
+        filename = config_dict['-new-fits']
+        fp = os.path.join(filedir, filename)
+        with open(fp, 'rb') as fd:
+            if ord(fd.read()) != 1:
+                raise AstrometryNetUnsolvedField(path)
+
     def solve(self, *args, **kwargs):
         args = self.update_args([self.path], args)
         options = self.update_kwargs(self.defaults, kwargs)
-        outpath = self.output(options)
-        output = self.run(*args, **options)
+        outpath = self.get_output_path(options)
+        output = self.sh(*args, **options)
+
         # We need to parse this output to determine whether or not
         # astrometry actually ran.
         return output
+        if output:
+            return fits.open(outpath)
