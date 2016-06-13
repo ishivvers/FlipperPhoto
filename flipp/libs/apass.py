c72f741531b531531b22801d04f5d0fe45f6d7f9
Thomas Tu <ttu@lbl.gov>
2016-06-12 17:11:28 -0700

    Docstrings for public query method

--- flipp/libs/apass.py
+++ flipp/libs/apass.py
@@ -37,9 +37,33 @@ class Client(object):
 
     @classmethod
     def query(cls, ra, dec, radius, outtype='1'):
-        ra = float(ra)
-        dec = float(dec)
-        radius = float(radius)
-        outtype = int(outtype)
+        """Queries the APASS database.
+
+        Notes
+        -----
+        Original download and AAVSO documentation can be found at
+        https://www.aavso.org/download-apass-data
+
+        Parameters
+        ----------
+        ra : str, float, int
+            Right Ascension in sexagesmial (HH:MM:SS, DD:MM:SS) or
+            decimal degrees (##.#)
+        dec : float
+            Declination in sexagesmial (HH:MM:SS, DD:MM:SS) or
+            decimal degrees (##.#)
+        radius : float
+            Radius to search around
+        outtype : int, optional (Default : 1)
+            This determines the datatype of the actual request to AAVSO
+            Currently, only handling for csv is available, so changing this
+            will break -- 0 for HTML, 1 for CSV
+
+        Returns
+        -------
+        pd.DataFrame
+            Pandas Dataframe of APASS search results.
+        """
         r = cls._get(ra, dec, radius, outtype)
+        assert r.status_code == 200, "Invalid query"
         return pd.read_csv(StringIO(r.text))
