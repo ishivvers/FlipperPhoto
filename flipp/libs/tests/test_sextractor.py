2016cb367ef7e4f26e039ffa5a796c4391165d02
Thomas Tu <ttu@lbl.gov>
2016-08-10 00:58:16 -0700

    Clean up a bit

--- flipp/libs/tests/test_sextractor.py
+++ flipp/libs/tests/test_sextractor.py
@@ -13,11 +13,16 @@ class TestSextractor(TestCase):
     def setUp(self):
         self.good_kait = os.path.join(FIXTURE_DIR, 'kait', 'goodkait.fits')
         self.bad_kait = os.path.join(FIXTURE_DIR, 'kait', 'badkait.fts.Z')
+        self.test_img = image_factory(1024)
+
+        self.S = SE.Sextractor()
+        self.extract = self.S.extract
 
     def test_extract(self):
-        self.assertTrue(True)
-        s = SE.Sextractor()
-        sources = s.extract(self.good_kait)
-        self.assertEquals(len(sources), 8) # Test real image
+        """Test various edge cases and use cases of SExtractor."""
 
-        self.assertEquals(len(s.extract(image_factory(1000))), 1) # Test dummy
+        gkait = self.extract(self.good_kait)
+        self.assertEquals(len(gkait), 8) # Test real image
+        test_img = self.extract(self.test_img)
+        self.assertEquals(len(test_img), 1) # Test dummy
+
