91a310e2dfb97498be440596ade0eadca1e1f8dd
Thomas Tu <ttu@lbl.gov>
2016-08-10 01:05:19 -0700

    add tests for dummy bad image

--- flipp/libs/tests/factories.py
+++ flipp/libs/tests/factories.py
@@ -9,13 +9,21 @@ from conf import FIXTURE_DIR
 
 GKAIT = os.path.join(FIXTURE_DIR, 'kait', 'goodkait.fits')
 
-def image_factory(length):
+def image_factory(length, peak='center'):
     """Creates a fake image with a single source object in the center."""
     data = np.zeros((length,)*2) # Create a L x L blank image
-    mean = [length//2.]*2 # Roughly center the image
+    if peak == 'center':
+        mean = [length//2.]*2 # Roughly center the image
+    elif peak == 'corner':
+        mean = [length]*2
+    elif peak == 'side':
+        mean = [length//2., length]
+    else:
+        raise ValueError("Peak must be one of 'center', 'corner' or 'side'")
     cov = [(np.sqrt(length)//2.,0), (0, np.sqrt(length)//2.)] # I think this is something
     x, y = np.random.multivariate_normal(mean, cov, int(1E5)).T
-    coords = zip(x.astype(int), y.astype(int))
+
+    coords = zip(x.astype(int)[x < length], y.astype(int)[y < length])
     for c in coords:
         data[c] += 1.
     hdu = fits.hdu.image.PrimaryHDU(data = data)
--- flipp/libs/tests/test_sextractor.py
+++ flipp/libs/tests/test_sextractor.py
@@ -13,7 +13,8 @@ class TestSextractor(TestCase):
     def setUp(self):
         self.good_kait = os.path.join(FIXTURE_DIR, 'kait', 'goodkait.fits')
         self.bad_kait = os.path.join(FIXTURE_DIR, 'kait', 'badkait.fts.Z')
-        self.test_img = image_factory(1024)
+        self.test_gimg = image_factory(1024) # Dummy good image
+        self.test_bimg = image_factory(1024, 'side') # Dummy bad image
 
         self.S = SE.Sextractor()
         self.extract = self.S.extract
@@ -23,6 +24,7 @@ class TestSextractor(TestCase):
 
         gkait = self.extract(self.good_kait)
         self.assertEquals(len(gkait), 8) # Test real image
-        test_img = self.extract(self.test_img)
-        self.assertEquals(len(test_img), 1) # Test dummy
-
+        test_gimg = self.extract(self.test_gimg)
+        self.assertEquals(len(test_gimg), 1) # Test good dummy image
+        test_bimg = self.extract(self.test_bimg)
+        self.assertEquals(len(test_bimg), 0)
