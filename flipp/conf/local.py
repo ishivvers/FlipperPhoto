2ea1740ae50f966b06771cd7b1856a2e77f25950
Thomas Tu <ttu@lbl.gov>
2016-09-30 12:08:59 -0700

    forgotten init with business logic

---
+++ flipp/conf/local.py
@@ -0,0 +1,1 @@
+DB_URL = "postgres://ttu:Tt3333334@localhost/flipp_db"
---
+++ flipp/pipeline/__init__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import unicode_literals
+
+from flipp.pipeline.image import ImageParser
+from flipp.pipeline.match import SourceMatcher
+
+
+def run(path_to_img, path_to_output, telescope):
+    """Business logic for running task.
+
+    Example
+    -------
+    .. code-block::
+        run("flipp/fixtures/kait/goodkait.fits",
+            "/home/ttu/Desktop/goodkait", telescope="kait")
+    """
+    img = ImageParser(path_to_img, path_to_output, telescope)
+    sources = I.run()
+    if not sources:
+        return
+    matcher = SourceMatcher(sources, img.META)
+    n_created, n_updated = matcher.run()
