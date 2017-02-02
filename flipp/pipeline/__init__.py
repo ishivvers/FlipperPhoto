4c570ced457db2059824c60108971ff4592ea6cd
Thomas Tu <ttu@lbl.gov>
2017-02-01 17:52:44 -0800

    Refactor flipprun

--- flipp/pipeline/__init__.py
+++ flipp/pipeline/__init__.py
@@ -2,18 +2,34 @@
 
 from __future__ import unicode_literals
 
+import os
 import gc
 import re
 import argparse
-
-from glob import glob
 
 from flipp.pipeline.image import ImageParser
 from flipp.pipeline.match import SourceMatcher
 
 from flipp.conf import settings
 
-def run(input_files, path_to_output=None, telescope=None, extensions=[]):
+
+def process_image(input_file, path_to_output, telescope):
+    try:
+        img = ImageParser(input_file, path_to_output, telescope)
+        sources = img.run()
+        if not sources:
+            return
+        matcher = SourceMatcher(img)
+        n_created, n_updated = matcher.run()
+        return n_created, n_updated
+    except Exception as e:
+        msg = "{} Failed with error: {}".format(input_file, e)
+        print(msg)
+    finally:
+        gc.collect()
+
+
+def run(input_paths, path_to_output=None, telescope=None, extensions=[], recursive=False):
     """Business logic for running task.
 
     Example
@@ -22,39 +38,48 @@ def run(input_files, path_to_output=None, telescope=None, extensions=[]):
         run("flipp/fixtures/kait/goodkait.fits",
             "/home/ttu/Desktop/goodkait", telescope="kait")
     """
-    R = '|'.join(map(re.escape, extensions)) + '$'
-    for input_file in input_files:
-        try:
-            if not re.search(R, input_file, flags=re.I):
+    path_to_output = os.path.abspath(path_to_output)
+    R = re.compile('|'.join(map(re.escape, extensions)) + '$', flags=re.I)
+    for input_path in input_paths:
+
+        input_path = os.path.abspath(input_path)
+        if os.path.isfile(input_path):
+
+            if not R.search(input_path):
                 continue
-            img = ImageParser(input_file, path_to_output, telescope)
-            sources = img.run()
-            if not sources:
-                continue
-            matcher = SourceMatcher(img)
-            n_created, n_updated = matcher.run()
-        except Exception as e:
-            msg = "{} Failed with error: {}".format(input_file, e)
-            print(msg)
-        gc.collect()
+            process_image(input_path, path_to_output, telescope)
+        else:  # os.path.isdir(input_path)
+            if not recursive:
+                for p in filter(os.path.isfile, os.listdir(input_path)):
+                    if not R.search(input_path):
+                        continue
+                    process_image(p, path_to_output, telescope)
+            else:  # recursive == True
+                for (name, dirs, files) in os.walk(input_path):
+                    for f in files:
+                        if not R.search(f):
+                            continue
+                        f = os.path.join(name, f)
+                        process_image(f, path_to_output, telescope)
 
 def console_run():
     """Console script entry-point for flipp pipeline."""
     parser = argparse.ArgumentParser(
         description='Run flipp pipeline on a file with some output directory.')
 
-    parser.add_argument("input_files", metavar= "input_files", type=str,
+    parser.add_argument("input_files", metavar= "file1 file2 ...", type=str,
         help="filepath to image.", nargs='+')
-    parser.add_argument("-o", "--output_dir", metavar="output_dir", type=str,
-        help = "Directory in which to save outputs")
-    parser.add_argument("-t", "--telescope", metavar = "telescope",
+    parser.add_argument("-o", "--output_dir", metavar="/path/to/output/directory", type=str,
+        help = "Directory in which to save outputs", default="outputs")
+    parser.add_argument("-t", "--telescope", metavar = "kait/nickel/etc",
         choices = settings.TELESCOPES.keys(),
         help = 'Telescope name from allowed names, {}'.format(
             ', '.join(settings.TELESCOPES)),
         default='kait'
         )
+    parser.add_argument("-r", "--recursive", action="store_true")
     parser.add_argument("-e", "--extensions", type=str, help="valid extensions", nargs = "*",
                         default=["fits", "fts", "fit", "fits.Z", "fts.Z", "fit.Z"])
 
     args = parser.parse_args()
-    run(args.input_files, args.output_dir, args.telescope, args.extensions)
+    run(args.input_files, args.output_dir, args.telescope, args.extensions, args.recursive)
